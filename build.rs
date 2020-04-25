2be628fe27ab2e8ce8a880f5a6a5b9191d582200
Rolfe Power <rolfepower4@gmail.com>
2020-04-25 14:56:16 -0400

    Moved the shell scripts to the `scripts` directory

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use std::process::Command;
 
 fn main() {
     Command::new("sh")
-        .arg("getspice.sh")
+        .arg("scripts/getspice.sh")
         .output()
         .expect("Failed to execute get spice process");
     println!("cargo:rustc-link-search=native=thirdparty/cspice/lib/");
