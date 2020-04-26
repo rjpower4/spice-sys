c4451cdae51176c4addf7222ad2edc67bbf5002e
Rolfe Power <rolfepower4@gmail.com>
2020-04-26 16:27:01 -0400

    tls --> lsk (proper name)

--- tests/load_unload_kernel.rs
+++ tests/load_unload_kernel.rs
@@ -5,11 +5,11 @@ use std::path::PathBuf;
 #[test]
 fn test_load_unload_kernel() {
     let spk_path: PathBuf = ["thirdparty", "testkernels", "de430.bsp"].iter().collect();
-    let tls_path: PathBuf = ["thirdparty", "testkernels", "naif0012.tls"]
+    let lsk_path: PathBuf = ["thirdparty", "testkernels", "naif0012.lsk"]
         .iter()
         .collect();
     let spk_kern_name = CString::new(spk_path.as_path().to_str().unwrap()).unwrap();
-    let tls_kern_name = CString::new(tls_path.as_path().to_str().unwrap()).unwrap();
+    let lsk_kern_name = CString::new(lsk_path.as_path().to_str().unwrap()).unwrap();
     let kern_type_all = CString::new("all").unwrap();
     let mut k = 0;
 
@@ -22,7 +22,7 @@ fn test_load_unload_kernel() {
         assert_eq!(k, 1);
 
         // Load the LSK
-        furnsh_c(tls_kern_name.as_ptr());
+        furnsh_c(lsk_kern_name.as_ptr());
 
         // Ensure that it was loaded
         ktotal_c(kern_type_all.as_ptr(), &mut k);
@@ -36,7 +36,7 @@ fn test_load_unload_kernel() {
         assert_eq!(k, 1);
 
         // Unload the LSK
-        unload_c(tls_kern_name.as_ptr());
+        unload_c(lsk_kern_name.as_ptr());
 
         // Ensure that it was unloaded
         ktotal_c(kern_type_all.as_ptr(), &mut k);
